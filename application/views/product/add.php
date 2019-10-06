4981765c15484b28f093ec014fbff57e38616550
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-10-06 23:47:12 +0700

    Add view products

---
+++ application/views/product/add.php
@@ -0,0 +1,74 @@
+            
+               <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                        <a class="btn btn-primary btn-raised pull-right waves-effect m-t--10" data-toggle="tooltip" data-original-title="Quay lại"  onclick=" window.history.back();">
+                                <i class="material-icons">keyboard_backspace</i> 
+                            </a>
+                            <h2>THÊM SẢN PHẨM</h2>
+                           
+                        </div>
+								
+                        <div class="body">
+                        
+                            <form id="form_advanced_validation" enctype="multipart/form-data"method="POST">
+                            <div class="row">
+                            
+                             <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+                                <div class="form-group form-float">
+                                <label class="form-label">Tên</label>
+                                    <div class="form-line">
+									<input type="text" class="form-control" placeholder="Tên sản phẩm" name="name" required>          
+                                    </div>
+                                </div>
+							</div>
+
+						
+							<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+								<div class="form-group form-float">
+								<label class="form-label">Giá bán</label>
+									<div class="form-line">
+									<input type="number" class="form-control" placeholder="Giá bán" name="price" required>
+									</div>
+								</div>
+								</div>
+								
+								
+								<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+								<div class="form-group form-float">
+								<label class="form-label">Tình trạng</label>
+									<div class="form-line">
+									<input type="text" class="form-control" placeholder="Tình trạng" name="status" >		
+									</div>							
+								</div>
+
+								</div>
+								<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+								<div class="form-group form-float">
+								<label class="form-label">Mô tả</label>
+									<div class="form-line">
+									<input type="text" class="form-control" placeholder="Mô tả" name="description" >									
+									</div>							
+								</div>
+								</div>	
+								<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+
+								<div class="form-group form-float">
+								<label class="form-label">Hình ảnh</label>
+									<div class="form-line">
+									<input type="file" class="form-control" placeholder="Hình ảnh" name="image" >									
+									</div>							
+								</div>
+								</div>	
+                             </div>
+                                <button class="btn btn-primary waves-effect btn-block" type="submit">LƯU</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+			</div>											
