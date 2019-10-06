65e108498259ae47dd41793f271b31e287ad98bc
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-10-06 23:48:57 +0700

    Add & edit function

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -10,9 +10,8 @@ class Product extends MY_Controller{
         parent::__construct();
         $this->load->model('Product_model');
         // $this->load->model('Bill_detail_model');
+         $this->load->library('upload');
     } 
-     
-
     /*
      * Listing of product
      */
@@ -24,4 +23,86 @@ class Product extends MY_Controller{
         $this->load->view('layouts/main',$data);
     }
     
+    function add()
+    {   
+        $this->form_validation->set_rules('name',"name",'required');
+        $this->form_validation->set_rules('price',"price",'required');
+        if($this->form_validation->run())
+        {
+        //     $config['upload_path'] = './upload';
+        // $config['allowed_types'] = 'gif|jpg|png';
+        //     $config['max_size'] = 2000;
+        //     $config['max_width'] = 1500;
+        //     $config['max_height'] = 1500;
+
+        //     $this->load->library('upload', $config);
+        // $this->upload->do_upload('upload');
+            if(isset($_FILES["image"]["name"]))  
+            { 
+                $config['upload_path'] = './public/images/';  
+                $config['allowed_types'] = 'gif|jpg|png|jpeg';  
+                $config['overwrite'] = true;  
+                // debug($_SESSION['user']); exit;
+                //$new_name = 'name_file';
+                $new_name  = 'file_name'.date('Ymd') . substr(microtime(), 2, 4) . md5(uniqid('', true));
+                $config['file_name'] = $new_name;
+                $this->load->library('upload');
+                $this->upload->initialize($config); 
+                
+                if($this->upload->do_upload('image'))  
+                {  
+                    $info = $this->upload->data();  
+                    $data =  "public/images/".$info['file_name'];
+                    $params = array(
+                        'name' => $this->input->post('name'),
+                        'price' => $this->input->post('price'),
+                        'status' => $this->input->post('status'),
+                        'description' => $this->input->post('description'),
+                        'image' => $data,
+                        'id'            => ObjectId()
+                    );
+                    //debug($data); exit;
+                     $product_id = $this->Product_model->add_product($params);
+                }
+            }
+            return redirect(base_url().'product');
+        }
+        else
+        {                   
+            $data['_view'] = 'product/add';
+            $this->load->view('layouts/main',$data);
+        }   
+    }  
+
+    function edit($id)
+    {   
+        $this->form_validation->set_rules('name',"name",'required');
+        $this->form_validation->set_rules('price',"price",'required');
+        // check if the product exists before trying to edit it
+        $data['product'] = $this->Product_model->get_product($id);
+        
+        if(isset($data['product']['id']))
+        {
+            if($this->form_validation->run()) 
+            {   
+                $params = array(
+                    'name' => $this->input->post('name'),
+                    'price' => $this->input->post('price'),
+                    'status' => $this->input->post('status'),
+                    'description' => $this->input->post('description'),
+                );
+
+                $this->Product_model->update_product($id,$params);            
+                return redirect(base_url().'product');
+
+            }
+            else
+            {
+                $data['_view'] = 'product/edit';
+                $this->load->view('layouts/main',$data);
+            }
+        }
+        else
+            show_error('The product you are trying to edit does not exist.');
+    }
 }
