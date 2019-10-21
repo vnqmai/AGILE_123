7dc7f925849beb3d078ff378180630acbb65b86d
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-10-21 17:29:45 +0700

    AGILE_123-31 Add subiz chatbox  & AGILE_123-32 Add footer

--- application/views/userlayouts/footer.php
+++ application/views/userlayouts/footer.php
@@ -1,5 +1,3 @@
- 
-       
     <!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
     <script src="<?=base_url()?>public/assets/plugins/bootstrap/js/bootstrap.js"></script>
     <script src="<?=base_url()?>public/assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
@@ -47,6 +45,104 @@
     <!-- Custom Theme Scripts -->
     <script src="<?=base_url()?>public/layouts/js/custom.min.js"></script>
 
-    
+    <a href="#top" class="smoothscroll scroll-top">
+        <span class="icon-keyboard_arrow_up"></span>
+      </a>
 
-    
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-6 col-md-2 mb-2 mb-md-0">
+            <h2 style="font-family:Tahoma; color:white">Công ty</h2>
+            <ul class="list-unstyled">
+              <li><a href="https://www.facebook.com">Facebook</a></li>
+              <li><a href="https://instagram.com">Instagram</a></li>
+              <li><a href="https://youtube.com">Youtube</a></li>
+              <li><a href="https://www.google.com">Google</a></li>
+            </ul>
+          </div>
+          <div class="col-6 col-md-3 mb-4 mb-md-1">
+            <h2 style="font-family:Tahoma; color:white">292 Food</h2>
+            <p>Với hơn 35 năm trong việc phục vụ nhu cầu ẩm thực cho mọi tầng lớp, chúng tôi luôn luôn mang đến cho khách hàng những cơ hội trải nghiệm khó quên từ các món ăn bổ dưỡng và nổi tiếng tại Việt Nam. Hãy đăng ký để nhận thực đơn mới nhất cùng chúng tôi, tại sao không?</p>
+          </div>
+
+          <!-- Begin Mailchimp Signup Form -->
+        <div class="col-6 col-md-3 mb-3 mb-md-0">
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+            <style type="text/css">
+                #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+            </style>
+            <div id="mc_embed_signup">
+            <form action="https://gmail.us20.list-manage.com/subscribe/post?u=27194df1efa9da27b4c14c282&amp;id=e6038ac8b1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+            <div class="indicates-required"><span class="asterisk">*</span> Bắt buộc</div>
+            <div class="mc-field-group">
+                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+            </label>
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-FNAME">First Name </label>
+                <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-LNAME">Last Name </label>
+                <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+            </div>
+            <!-- <div class="mc-field-group size1of2">
+                <label for="mce-BIRTHDAY-month">Birthday </label>
+                <div class="datefield">
+                    <span class="subfield monthfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="MM" size="2" maxlength="2" name="BIRTHDAY[month]" id="mce-BIRTHDAY-month"></span> / 
+                    <span class="subfield dayfield"><input class="birthday " type="text" pattern="[0-9]*" value="" placeholder="DD" size="2" maxlength="2" name="BIRTHDAY[day]" id="mce-BIRTHDAY-day"></span> 
+                    <span class="small-meta nowrap">( mm / dd )</span>
+                </div>
+            </div>   -->
+            <div id="mce-responses" class="clear">
+                    <div class="response" id="mce-error-response" style="display:none"></div>
+                    <div class="response" id="mce-success-response" style="display:none"></div>
+                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_27194df1efa9da27b4c14c282_e6038ac8b1" tabindex="-1" value=""></div>
+                <div class="clear"><input type="submit" value="Đăng ký" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                </div>
+            </form>
+            </div>
+            <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';fnames[5]='BIRTHDAY';ftypes[5]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+        </div>
+<!--End mc_embed_signup-->
+
+<div class="col-6 col-md-2 mb-3 mb-md-0" style="padding-left: 35px; width: 160px;">
+    <div class="footer-titles">Giấy phép</div>
+        <a>MXH 363/GP-BTTTT</a>
+        <a target="_blank" rel="nofollow" href="http://online.gov.vn/HomePage/WebsiteDisplay.aspx?DocId=13113">
+            <img src="public/assets/images/gov_seals.jpg" width="160" style="margin-left: -5px;" alt="logo-da-dang-ky-bo-cong-thuong">
+        </a>
+                            
+    </div>
+</div>
+
+        <div class="row text-center">
+          <div class="col-12">
+            <p class="copyright">
+            <!-- Link back to Free-Template.co can't be removed. Template is licensed under CC BY 3.0. -->
+            <p class="block">&copy; <script>document.write(new Date().getFullYear());</script> <strong class="text-white">292 Food</strong> Nhom 3. All Rights Reserved. <br> Designed &amp; Developed by Nhom 3</p>
+            </p>
+          </div>
+        </div>
+      </div>    
+
+<!-- Subiz -->
+        <script>
+            (function(s, u, b, i, z){
+                u[i]=u[i]||function(){
+                    u[i].t=+new Date();
+                        (u[i].q=u[i].q||[]).push(arguments);
+                    };
+                    z=s.createElement('script');
+                    var zz=s.getElementsByTagName('script')[0];
+                    z.async=1; z.src=b; z.id='subiz-script';
+                    zz.parentNode.insertBefore(z,zz);
+                })(document, window, 'https://widgetv4.subiz.com/static/js/app.js', 'subiz');
+                subiz('setAccount', 'acqmebjsxywvavzewfqf');
+        </script>
+<!-- End Subiz -->
