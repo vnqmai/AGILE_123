1fee5c93bfd721ef21b7bd135be3fc0e86e76833
yennhi2404 <44503532+yennhi2404@users.noreply.github.com>
2019-11-04 18:49:10 +0700

    Fix login icon

--- application/views/userlayouts/navbar.php
+++ application/views/userlayouts/navbar.php
@@ -1,3 +1,4 @@
+
 <div class="navbar nav_title" style="border: 0;">
   <a class="site_title" href="order"> <i class="material-icons" style="font: Courier">account_balance</i><span> 292 Food</span></a>
 </div>
@@ -5,27 +6,30 @@
 <div class="top_nav">
   <div class="nav_menu">
             <nav>
-
               <div class="nav toggle">
                 <img src="<?=base_url()?>public/images/headerbanner.png" alt="Hinh_banner_quang_cao" width="1030px" height="220px">
               </div>
-             
+             <?php
+                  $CI =& get_instance();
+                  $CI->load->model('User_model');
+                  $user = $CI->User_model->get_user($_SESSION['user']->id);
+            ?>
               <ul class="nav navbar-nav navbar-right">
                 <li class="">
                   <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                    <img src="<?=base_url()?>public/assets/images/user.png" alt="">
+                    <img src="<?=base_url()?>public/assets/images/user.png" alt=""><?= $user['firstName'].' '.$user['lastName']; ?>
                     <i class="material-icons">arrow_drop_down</i>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <!-- <li><a href=""> <i class="material-icons">face</i> Profile </a></li> -->
+                    <!-- <li><a href="<?=base_url()?>staff/profile/<?=$_SESSION['user']->id?>"> <i class="material-icons">face</i> Profile </a></li> -->
                     
                    
-                    <li><a href="#"><i class="material-icons">exit_to_app</i> Log Out</a></li>
+                    <li><a href="<?=base_url()?>auth/logout"><i class="material-icons">exit_to_app</i> Log Out</a></li>
                   </ul>
                 </li>
 
                 
               </ul>
             </nav>
-  </div>
+          </div>
 </div>
