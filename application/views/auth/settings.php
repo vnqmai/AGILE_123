f79cda442f571b00c6661dda2e953170d78c5668
tainm <56074186+tainm@users.noreply.github.com>
2019-11-04 16:31:32 +0700

    A2-7: Sign-in
    Sign-in website using google account

---
+++ application/views/auth/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '103672694883-oh5bkapgau4u1g7e3gagu4o8lcu6i8v7.apps.googleusercontent.com ');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'jSmkrHRLAKnkJcb21I_W8Ui9 ');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/agile_123/application/controllers/oauth.php');
+
+?>
